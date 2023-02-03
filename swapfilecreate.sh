63a1afbf5edfb33dfd3d93b34e9047f789be0946
root <root@test1-mail100-pretest001.emdad-tech.com>
2023-02-03 22:59:59 +0000

    small changes

--- swapfilecreate.sh
+++ swapfilecreate.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+# This tool programmatically create a swap file in linux
+# you can use it and provide it with the swap file size the the swap file location 
+# and it will create the swap file
+#
+#
+#MIT License
+#Copyright (c) 2023 Mohammed AlShannaq
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 
 swap_file_location=/swapfile1
 
@@ -78,3 +103,4 @@ echo "writing swap file defintion to /etc/fstab"
 echo "$swap_file_location          swap            swap    defaults        0 0" >> /etc/fstab
 echo "Looking for Memory size"
 free -mh
+exit 0
